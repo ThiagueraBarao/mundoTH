5ef55a5a0eb4ed4c650fcc129dc3642ff45f6f4d
Thiago do Carmo Nunes <thiagocarmonunes@hotmail.com>
2021-01-03 11:04:37 -0300

    React Refatorado

--- App.js
+++ App.js
@@ -2,13 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import PrimeiroComponente from './src/PrimeiroComponente';
+
+export default class App extends React.Component  {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  render (){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <PrimeiroComponente text = "Agora foi"/>
+
+      <PrimeiroComponente text = "Agora foi 3"/>
+
+      <PrimeiroComponente text = "Agora foi 3"/>
+
     </View>
   );
+}
+
 }
 
 const styles = StyleSheet.create({
